37a179f1f9f6dd77fb1752fb027e746fda050d8d
zohreh okhravi <zohrehbaghri2265@gmail.com>
2024-08-16 21:10:22 +0330

    تمرین

---
+++ exercise3/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$name= array("zohreh","pooria","mahdi","atena","amirali","kiamehr","nima","arash");
+echo sizeof($name);
+$i=0;
+while ($i<sizeof($name)){
+    echo $name[$i];
+    echo "<br/>";
+    $i++;
+}
+
+echo "<hr/>";
+
+for($i=0;$i<29;$i++){
+    echo "<p>Hi</p>";
+}
+
+?>
