dce931757b8343ad406984032d65fd9af75d3cc4
zohreh okhravi <zohrehbaghri2265@gmail.com>
2024-08-16 21:09:23 +0330

    تمرین

---
+++ exercise2/index.php
@@ -0,0 +1,51 @@
+<?php
+
+$numbers= array(2,17.5,9.75,15,19);
+
+
+if($numbers[0] >=10){
+    echo "$numbers[0] passed";
+    echo "<br>";
+}
+else{
+    echo "numbers[0] failed";
+    echo "<br>";
+}
+
+if($numbers[1] >=10){
+    echo "$numbers[1] passed";
+    echo "<br>";
+}
+else{
+    echo "numbers[1] failed";
+    echo "<br>";
+}
+
+if($numbers[2] >=10){
+    echo "$numbers[2] passed";
+    echo "<br>";
+}
+else{
+    echo "numbers[2] failed";
+    echo "<br>";
+}
+
+if($numbers[3] >=10){
+    echo "$numbers[3] passed";
+    echo "<br>";
+}
+else{
+    echo "numbers[3] failed";
+    echo "<br>";
+}
+
+if($numbers[4] >=10){
+    echo "$numbers[4] passed";
+    echo "<br>";
+}
+else{
+    echo "numbers[4] failed";
+    echo "<br>";
+}
+
+?>
