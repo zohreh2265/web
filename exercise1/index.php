d3ebf371fa44d00f76b39608db71cd8bdfb9fa57
zohreh okhravi <zohrehbaghri2265@gmail.com>
2024-08-16 21:07:54 +0330

    تمرین

---
+++ exercise1/index.php
@@ -0,0 +1,43 @@
+<?php
+$animals= array(
+    'one'=>array("zohreh","okhravi",20,52,1.55,false),
+    'two'=>array("Morteza", "Ghoddosui", 30, 92, 1.86, true),
+    'three'=>array("Pooria", "Nadimi", 20, 85, 1.76, true),
+    'four'=>array("Atena", "Asadi", 19, 75, 1.70, false),
+    'five'=>array("AmirAli", "MasoumShahi", 20, 53, 1.71, true),
+    'six'=>array("Kiamehr", "Keramati", 13, 51, 1.60, true),
+    'seven'=>array("Nima", "Javanshir", 17, 86, 1.70, true),
+    'eight'=>array("Arash", "HasanPour", 15, 77, 1.66, true),
+    'nain'=>array("Mahdi", "Khayatan", 17, 56, 1.70, true)
+    );
+    print_r($animals['one']);
+    echo "<br />";
+
+    // echo $animals['one'][0];   
+    // echo "<br />";
+    
+    // echo $animals['two'][1];   
+    // echo "<br />";
+
+    // echo $animals['three'][2];   
+    // echo "<br />";
+
+    // echo $animals['four'][3];   
+    // echo "<br />";
+
+    // echo $animals['five'][4];   
+    // echo "<br />";
+
+    // echo $animals['six'][5];   
+    // echo "<br />";
+
+    // echo $animals['seven'][1];   
+    // echo "<br />";
+
+    // echo $animals['eight'][0];   
+    // echo "<br />";
+
+    // echo $animals['nain'][4];   
+    // echo "<br />";
+
+?>
